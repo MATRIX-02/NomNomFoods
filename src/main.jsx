b535c602a6669d4598a0a844d11400e303cac121
Mayank <mayanksingh12@gmail.com>
2024-02-03 15:26:21 +0530

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const appRouter = createBrowserRouter([
+	{
+		path: "/NomNomFoods",
+		element: <App/>,
+	},
+	
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter}/>
   </React.StrictMode>,
 )
