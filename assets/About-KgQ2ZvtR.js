d45d69002c587135295914eca7a7051e4ff33559
MATRIX-02 <MATRIX-02@users.noreply.github.com>
2024-02-12 13:47:54 +0000

    deploy: bbbb8d4758e9eb8ca9fa0a1a9ab3fd8848eaa42f

---
+++ assets/About-KgQ2ZvtR.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-4srymM3j.js";const t=()=>s.jsxs("div",{children:[s.jsx("h1",{children:"About"}),s.jsx("h2",{children:"This is Namaste React Web Series"})]});export{t as default};
