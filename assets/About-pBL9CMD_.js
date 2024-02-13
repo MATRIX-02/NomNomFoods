6e9f4ee2e93472e80112532449f59f9326ce7f80
MATRIX-02 <MATRIX-02@users.noreply.github.com>
2024-02-13 06:10:59 +0000

    deploy: 14f052eee2f089ab2b448adebfc89dc031b78515

--- assets/About-pBL9CMD_.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Fjntarng.js";const t=()=>s.jsxs("div",{children:[s.jsx("h1",{children:"About"}),s.jsx("h2",{children:"This is Namaste React Web Series"})]});export{t as default};
