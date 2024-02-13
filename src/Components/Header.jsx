103f2e228010de49e075fbffbe42b09aed84943d
Mayank <mayanksingh12@gmail.com>
2024-02-13 22:56:44 +0530

    add: favicon; fix: Logo size fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
     <div className="h-20 flex justify-between items-center px-32 fixed inset-0 bg-white z-50 shadow-md">
       <Link to="/NomNomFoods">
         <img
-          className="w-20 h-auto"
+          className="w-20 h-auto fixed top-0"
           src="./Images/Logo.png"
           alt="NomNomFoods"
         />
