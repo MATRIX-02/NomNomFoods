d8a4e4d048f223393cdbf09258ffecf064e4eae3
Mayank <mayanksingh12@gmail.com>
2024-03-19 00:10:20 +0530

    refactor: updated cors  Not working on deployed website but working in localhost

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -2,8 +2,7 @@ export const LAT = "28.6166063";
 
 export const LNG = "77.1300404";
 
-// export const PROXY_CORS = "https://thingproxy.freeboard.io/fetch/";
-export const PROXY_CORS = "";
+export const PROXY_CORS = "https://thingproxy.freeboard.io/fetch/";
 
 export const generateProxyUrl = (URL) => PROXY_CORS + encodeURIComponent(URL)
 
