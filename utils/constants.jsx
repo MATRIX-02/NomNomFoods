2e0638a41b63114a1d6a5cd0d3242bb75ed27ded
Mayank <mayanksingh12@gmail.com>
2024-05-11 09:17:37 +0530

    refactor: Cloudinary image url updated

--- utils/constants.jsx
+++ utils/constants.jsx
@@ -9,7 +9,7 @@ export const generateProxyUrl = (URL) => PROXY_CORS + URL
 export const SWIGGY_API = "api/restaurants?lat=" + LAT + "&lng=" + LNG + "0000001&page_type=DESKTOP_WEB_LISTING";
 
 export const CDN_URL =
-	"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL =
 	"https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018";
